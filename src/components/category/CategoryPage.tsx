ba7dbaecc2f80609c66025ebd1233ca27337fe46
Kaushallrai <kaushall.rai@gmail.com>
2024-12-19 21:56:57 +0545

    git commit

--- src/components/category/CategoryPage.tsx
+++ src/components/category/CategoryPage.tsx
@@ -1,3 +1,11 @@
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "../ui/breadcrumb";
+
 interface Category {
   id: number;
   name: string;
@@ -9,8 +17,25 @@ interface CategoryPageProps {
 
 export default function CategoryPage({ category }: CategoryPageProps) {
   return (
-    <div className="container mx-auto py-10">
-      <h1 className="text-3xl font-bold mb-5">{category.name}</h1>
+    <div className="mt-4 ">
+      <div>
+        <h1 className="text-2xl font-semibold ">{category.name}</h1>
+        <div className="flex h-12 shrink-0 items-center gap-2 ">
+          <div className="flex items-center gap-2 ">
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem className="hidden md:block">
+                  Category
+                </BreadcrumbItem>
+                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbItem>
+                  <BreadcrumbPage>{category.name}</BreadcrumbPage>
+                </BreadcrumbItem>
+              </BreadcrumbList>
+            </Breadcrumb>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
