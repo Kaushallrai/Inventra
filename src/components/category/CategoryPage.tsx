077c068833c9f339b689a366da652d5e60b244c5
Kaushallrai <kaushall.rai@gmail.com>
2024-12-20 21:31:20 +0545

    add brand button

--- src/components/category/CategoryPage.tsx
+++ src/components/category/CategoryPage.tsx
@@ -1,3 +1,4 @@
+import { PlusSquare } from "lucide-react";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -5,6 +6,7 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "../ui/breadcrumb";
+import { Button } from "../ui/button";
 
 interface Category {
   id: number;
@@ -18,22 +20,30 @@ interface CategoryPageProps {
 export default function CategoryPage({ category }: CategoryPageProps) {
   return (
     <div className="mt-4 ">
-      <div>
-        <h1 className="text-2xl font-semibold ">{category.name}</h1>
-        <div className="flex h-12 shrink-0 items-center gap-2 ">
-          <div className="flex items-center gap-2 ">
-            <Breadcrumb>
-              <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  Category
-                </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
-                <BreadcrumbItem>
-                  <BreadcrumbPage>{category.name}</BreadcrumbPage>
-                </BreadcrumbItem>
-              </BreadcrumbList>
-            </Breadcrumb>
+      <div className="flex items-center justify-between">
+        <div>
+          <h1 className="text-2xl font-semibold ">{category.name}</h1>
+          <div className="flex h-12 shrink-0 items-center gap-2 ">
+            <div className="flex items-center gap-2 ">
+              <Breadcrumb>
+                <BreadcrumbList>
+                  <BreadcrumbItem className="hidden md:block">
+                    Category
+                  </BreadcrumbItem>
+                  <BreadcrumbSeparator className="hidden md:block" />
+                  <BreadcrumbItem>
+                    <BreadcrumbPage>{category.name}</BreadcrumbPage>
+                  </BreadcrumbItem>
+                </BreadcrumbList>
+              </Breadcrumb>
+            </div>
           </div>
+        </div>
+        <div>
+          <Button className="bg-blue-600 hover:bg-blue-500 text-white">
+            <PlusSquare />
+            Add Brand
+          </Button>
         </div>
       </div>
     </div>
